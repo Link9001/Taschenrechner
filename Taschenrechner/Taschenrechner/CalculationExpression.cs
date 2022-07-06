661f48748c145a1d0d0d3612f80a8af2e00831d6
Link9001 <89010971+Link9001@users.noreply.github.com>
2022-07-06 07:54:30 +0200

    Add base Calculator

---
+++ Taschenrechner/Taschenrechner/CalculationExpression.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    public class CalculationExpression
+    {
+        public CalculationOperations? Operation { get; set; }
+
+        private double? secondNumber;
+        private double? firstNumber;
+
+        public double FirstNumber => secondNumber ?? 0;
+
+        public double SecondNumber => secondNumber ?? 0;
+
+        public CalculationExpression(double firstNumber)
+        {
+            this.firstNumber = firstNumber;
+        }
+
+        public void SetNumber(double number)
+        {
+            if (Operation == null)
+            {
+                firstNumber = number;
+            }
+            else
+            {
+                secondNumber = number;
+            }
+        }
+
+        public double Caluculate()
+        {
+            switch (Operation)
+            {
+                case CalculationOperations.Addition:
+                    return FirstNumber + SecondNumber;
+                case CalculationOperations.Substraktion:
+                    return FirstNumber - SecondNumber;
+                case CalculationOperations.Multiplication:
+                    return FirstNumber * SecondNumber;
+                case CalculationOperations.Division:
+                    if (FirstNumber != 0 && SecondNumber != 0)
+                    {
+                        return FirstNumber / SecondNumber;
+                    }
+
+                    return double.NaN;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public enum CalculationOperations
+        {
+            Addition = 0,
+            Substraktion = 1,
+            Multiplication = 2,
+            Division = 3,
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstNumber} {Operation} {SecondNumber}";
+        }
+    }
+}
---
+++ Taschenrechner/Taschenrechner/MainPageViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Taschenrechner
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty] 
+        private double result;
+
+        private CalculationExpression expression;
+
+        private string number = string.Empty;
+        private readonly List<CalculationExpression> expressions = new List<CalculationExpression>();
+
+        public CalculationExpression CurrentExpression
+        {
+            get => expressions.Last();
+            set
+            {
+                if (SetProperty(ref expression, value))
+                {
+                    expressions.Add(value);
+                    expression = value;
+                }
+            }
+        }
+
+        [ICommand]
+        private void Add()
+        {
+            CurrentExpression.SetNumber(double.Parse(number));
+            CurrentExpression.Operation = CalculationExpression.CalculationOperations.Addition;
+            number = string.Empty;
+        }
+
+        [ICommand]
+        private void Sub()
+        {
+            CurrentExpression.SetNumber(double.Parse(number));
+            CurrentExpression.Operation = CalculationExpression.CalculationOperations.Substraktion;
+            number = string.Empty;
+        }
+
+        [ICommand]
+        private void Mult()
+        {
+            CurrentExpression.SetNumber(double.Parse(number));
+            CurrentExpression.Operation = CalculationExpression.CalculationOperations.Multiplication;
+            number = string.Empty;
+        }
+
+        [ICommand]
+        private void Div()
+        {
+            CurrentExpression.SetNumber(double.Parse(number));
+            CurrentExpression.Operation = CalculationExpression.CalculationOperations.Division;
+            number = string.Empty;
+        }
+        [ICommand]
+        private void Dot()
+        {
+            number += ".";
+        }
+
+        [ICommand]
+        private void One()
+        {
+            number += "1";
+        }
+        [ICommand]
+        private void Two()
+        {
+            number += "2";
+        }
+        [ICommand]
+        private void Three()
+        {
+            number += "3";
+        }
+        [ICommand]
+        private void Four()
+        {
+            number += "4";
+        }
+        [ICommand]
+        private void Five()
+        {
+            number += "5";
+        }
+        [ICommand]
+        private void Six()
+        {
+            number += "6";
+        }
+        [ICommand]
+        private void Seven()
+        {
+            number += "7";
+        }
+        [ICommand]
+        private void Eight()
+        {
+            number += "8";
+        }
+        [ICommand]
+        private void Nine()
+        {
+            number += "9";
+        }
+        [ICommand]
+        private void Zero()
+        {
+            number += "0";
+        }
+
+        [ICommand]
+        private void Equal()
+        {
+            foreach (var calculationExpression in expressions)
+            {
+                result += calculationExpression.Caluculate();
+            }
+        }
+    }
+}
